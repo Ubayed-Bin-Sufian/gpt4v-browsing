b375b076f437f322288a13d1751e4b62f870827a
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-10-09 00:09:03 +0600

    System prompt added

--- flask_chatgpt.py
+++ flask_chatgpt.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-import requests
 from openai import OpenAI
 
 client = OpenAI()
@@ -10,18 +9,17 @@ app = Flask(__name__)
 def welcome():
     return "Welcome Ubayed"
 
-#print(completion.choices[0].message)
-
 @app.route("/answer", methods=["GET", "POST"])
 # Function to call OpenAI's ChatGPT API
 def call_openai_api():
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "system", "content": "You are an AI assistant specializing in providing the latest news and updates in the field of artificial intelligence, including breakthroughs, research, industry trends, and important developments. Provide accurate and concise information about the most recent events in AI, and prioritize the most relevant news."},
             {"role": "user", "content": "Hello!"}
         ],
         completion=True
+        #print(completion.choices[0].message)
     ) 
 
     for chunk in completion:
