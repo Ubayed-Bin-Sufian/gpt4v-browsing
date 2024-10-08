324d33d41eb609661c12a519836c6d95b9264f69
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-10-09 03:11:40 +0600

    system and user role added

--- info_from_chatgpt.py
+++ info_from_chatgpt.py
@@ -1,17 +1,18 @@
-import openai
+from openai import OpenAI
 
-openai.api_key = ""
-
-# Print or use the API key
-print(f"Your OpenAI API Key is: {openai.api_key}")
+OPENAI_KEY=""
+client = OpenAI(api_key=OPENAI_KEY)
 
 # Define a function to interact with GPT
 def chat_with_gpt(prompt):
 
     # Call the OpenAI API with the provided prompt
-    response = openai.ChatCompletion.create(
+    response = client.chat.completions.create(
       model="gpt-4o-mini",
-      messages=[{"role": "user", "content": prompt}]
+      messages=[
+            {"role": "system", "content": "You are an AI assistant specializing in providing the latest news and updates in the field of artificial intelligence, including breakthroughs, research, industry trends, and important developments. Provide accurate and concise information about the most recent events in AI, and prioritize the most relevant news."},
+            {"role": "user", "content": prompt}
+        ]
     )
    
     # Return the generated response after stripping whitespace
